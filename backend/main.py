7040de99f13330f6192429e26f8c9d1a6295ff09
Francesco Grassi <grassifrancesco07@protonmail.com>
2026-01-16 20:38:52 +0100

    ripulito main.py ed aggiunto dei TODO

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
+import random
+import sqlite3
 import uuid
 
 from faker import Faker
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from markupsafe import escape
 
 app = Flask(__name__)
 CORS(app)
@@ -16,26 +17,35 @@ def index():
     return "INDEX DELLA API, VAI SU /api/libri PER OTTENERE DEI LIBRI"
 
 
-libri = []  # INIZIALIZZO IL VETTORE LIBRI (SAREBBE MEGLIO USARE UN DATABASE)
+libri = []  # INIZIALIZZO IL VETTORE LIBRI (TODO: SOSTITUIRE CON UN DATABASE)
+generi = [
+    "Fantasy",
+    "Horror",
+    "Thriller",
+    "Romanzo",
+    "Storico",
+    "Fantascienza",
+    "Biografia",
+]
 
 
 @app.route("/api/libri", methods=["GET"])
 def get_libri():
-    for i in range(20):  # CREO 20 LIBRI
-        libro = {
-            "id": str(uuid.uuid4()),
-            "titolo": fake.sentence(nb_words=3).rstrip(".").title(),
-            "autore": fake.name(),
-            "anno": fake.year(),
-            "genere": fake.random_element(elements=("M", "F")),
-        }
+    # for i in range(20):  # CREO 20 LIBRI
+    #     libro = {
+    #         "id": str(uuid.uuid4()),
+    #         "titolo": fake.sentence(nb_words=3).rstrip(".").title(),
+    #         "autore": fake.name(),
+    #         "anno": fake.year(),
+    #         "genere": fake.random_element(elements=("M", "F")),
+    #     }
 
-        libri.append(
-            libro
-        )  # AGGIUNGO IL LIBRO CREATO AL VETTORE (MA SAREBBE MEGLIO AGGIUNGERLO AD UN DATABASE)
+    #     libri.append(
+    #         libro
+    #     )  # AGGIUNGO IL LIBRO CREATO AL VETTORE (TODO: SOSTITUIRE CON UN DATABASE)
 
     return jsonify(
-        {  # PRENDO UN FILE .json CONTENENTE I LIBRI
+        {  # RITORNO UN FILE .json CONTENENTE TUTTI I LIBRI PRESENTI NEL VETTORE (TODO: SOSTITUIRE CON UN DATABASE)
             "success": True,
             "data": libri,
         }
@@ -54,17 +64,17 @@ def post_libro():
         or "anno" not in libro
         or "genere" not in libro
     ):
-        return jsonify({"success": False, "error": "Dati mancanti"}), 400
+        return jsonify({"success": False, "error": "Dati mancanti!!!!!!"}), 400
 
     nuovo_libro = {
-        "id": str(uuid.uuid4()),
+        "id": str(uuid.uuid4()),  # COME FACCIO A GENERARE UN ID USANDO IL DATABASE?
         "titolo": libro["titolo"],
         "autore": libro["autore"],
         "anno": libro["anno"],
         "genere": libro["genere"],
     }
 
-    libri.append(nuovo_libro)
+    libri.append(nuovo_libro)  # TODO: SOSTITUIRE CON UN DATABASE
 
     return jsonify({"success": True, "data": nuovo_libro}), 201
 
